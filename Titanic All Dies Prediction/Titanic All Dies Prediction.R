c36560e6acd838db1d81b54d96746c6aa60e8178
Anirban Sarkar <147172432+anirban-sarkar-2046@users.noreply.github.com>
2024-10-31 01:37:48 +0600

    Delete Titanic All Dies Prediction/Titanic All Dies Prediction.R

--- Titanic All Dies Prediction/Titanic All Dies Prediction.R
+++
@@ -1,5 +0,0 @@
-setwd("C:/Users/Anirban/Documents/R Studio Projects/Datasets/Titanic - Machine Learning from Disaster")
-
-titanic.train <- read.csv(file = "train.csv", stringsAsFactors = FALSE, header = TRUE)
-titanic.test <- read.csv(file = "test.csv", stringsAsFactors = FALSE, header = TRUE)
-
